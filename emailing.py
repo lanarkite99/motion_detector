9bfd11cf9ec40c33466413eb50a0191372be7010
Meet <meetapple191@gmail.com>
2024-06-10 18:52:23 +0530

    Motion detection using sliding window method

--- emailing.py
+++ emailing.py
@@ -1,2 +1,29 @@
-def send_email():
-    print("Email sent")
+import smtplib, os, ssl
+from email.message import EmailMessage
+import imghdr
+
+SENDER = "meetapple191@gmail.com"
+PASSWORD = os.getenv("py_web")
+RECEIVER = "meetsiddhapura@gmail.com"
+HOST = "smtp.gmail.com"
+PORT = 587
+
+def send_email(image_path):
+    print("Email sending started")
+    email_message=EmailMessage()
+    email_message["Subject"]="New Customer has entered!!"
+    email_message.set_content("Hi, Can you assist the new customer")
+
+    with open(image_path,"rb") as file:
+        content=file.read()
+    email_message.add_attachment(content,maintype="image", subtype=imghdr.what(None,content))
+
+    gmail=smtplib.SMTP(HOST,PORT)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER,RECEIVER,email_message.as_string())
+    gmail.quit()
+    print("Email sent ")
+if __name__=="__main__":
+    send_email(image_path="images/10.png")
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import glob,os,time
 import cv2
-import time
 from emailing import send_email
+from threading import Thread
 
 video=cv2.VideoCapture(0,cv2.CAP_DSHOW)
 time.sleep(1)
 
 first_frame=None
 status_list=[]
+sent_image=[]
+count=0
+
+def clean_dir():
+    images=glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status=0
@@ -30,12 +38,24 @@ while True:
         rectangle=cv2.rectangle(frame,(x,y), (x+w,y+h), (0,255,0),3)
         if rectangle.any():
             status=1
+            cv2.imwrite(f"images/{count}.png",frame)
+            count=count+1
+            all_images=glob.glob("images/*.png")
+            sorted_images=sorted(all_images,key=os.path.getmtime)
+            index=int(len(sorted_images)/2)
+            image_with_object=sorted_images[index]
+
+
 
     status_list.append(status)
     status_list=status_list[-2:]
 
     if status_list[0]==1 and status_list[1]==0:
-        send_email()
+        if image_with_object not in sent_image:
+            sent_image.append(image_with_object)
+            email_thread=Thread(target=send_email,args=(image_with_object, ))
+            email_thread.daemon=True
+            email_thread.start()
 
     cv2.imshow("Video",frame)
 
@@ -44,5 +64,12 @@ while True:
         break
 
 video.release()
+
+print("Folder cleaning started")
+clean_thread = Thread(target=clean_dir())
+clean_thread.daemon = True
+clean_thread.start()
+clean_thread.join()
+print("Folder cleaning ended")
 # array=cv2.imread("image.png")
 # print(array)
